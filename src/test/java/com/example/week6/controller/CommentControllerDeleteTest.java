ddeaeb5ea95e1d312c78675162bca7b598eea6af
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-09-06 01:21:38 +0900

    test code

---
+++ src/test/java/com/example/week6/controller/CommentControllerDeleteTest.java
@@ -0,0 +1,73 @@
+package com.example.week6.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class CommentControllerDeleteTest {
+
+    @Nested
+    @DisplayName("삭제 - 실패")
+    class CreateComment{
+        @Test
+        @DisplayName("Refresh-Token")
+        void A() {
+            //given
+
+            //when
+
+            //then
+        }
+
+        @Test
+        @DisplayName("Authorization")
+        void B() {
+            //given
+
+            //when
+
+            //then
+        }
+
+        @Test
+        @DisplayName("member is null")
+        void C() {
+            //given
+
+            //when
+
+            //then
+        }
+
+        @Test
+        @DisplayName("Comment is null")
+        void D() {
+            //given
+
+            //when
+
+            //then
+        }
+
+        @Test
+        @DisplayName("Member가 다름")
+        void E() {
+            //given
+
+            //when
+
+            //then
+        }
+
+    }
+
+    @Test
+    @DisplayName("삭제- success")
+    void A() {
+        //given
+
+        //when
+
+        //then
+    }
+}
---
+++ src/test/java/com/example/week6/controller/CommentControllerUpdateTest.java
@@ -0,0 +1,83 @@
+package com.example.week6.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class CommentControllerUpdateTest {
+
+    @Nested
+    @DisplayName("생성 테스트")
+    class UpdateComment{
+        @Test
+        @DisplayName("Refresh-Token")
+        void A() {
+            //given
+
+            //when
+
+            //then
+        }
+
+        @Test
+        @DisplayName("Authorization")
+        void B() {
+            //given
+
+            //when
+
+            //then
+        }
+
+        @Test
+        @DisplayName("member is null")
+        void C() {
+            //given
+
+            //when
+
+            //then
+        }
+
+        @Test
+        @DisplayName("post is null")
+        void D() {
+            //given
+
+            //when
+
+            //then
+        }
+
+        @Test
+        @DisplayName("comment is null")
+        void E() {
+            //given
+
+            //when
+
+            //then
+        }
+
+        @Test
+        @DisplayName("member가 다를 경우")
+        void F() {
+            //given
+
+            //when
+
+            //then
+        }
+
+    }
+
+    @Test
+    @DisplayName("Success- Comment")
+    void A() {
+        //given
+
+        //when
+
+        //then
+    }
+}
