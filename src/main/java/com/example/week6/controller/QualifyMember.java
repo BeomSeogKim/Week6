a602686d06e890982a994379462be207bc8d0fbf
BeomSeogKim <kbs4520@daum.net>
2022-09-07 16:54:51 +0900

    Refactoring

---
+++ src/main/java/com/example/week6/controller/QualifyMember.java
@@ -0,0 +1,10 @@
+package com.example.week6.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class QualifyMember {
+
+}
