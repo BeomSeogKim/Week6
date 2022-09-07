08262eae50cf07be037e01eb4332aef3548405fb
BeomSeogKim <kbs4520@daum.net>
2022-09-07 16:57:40 +0900

    Refactoring for MemberController

--- src/main/java/com/example/week6/controller/MemberController.java
+++ src/main/java/com/example/week6/controller/MemberController.java
@@ -22,18 +22,28 @@ public class MemberController {
 
   private final MemberService memberService;
 
+  /**
+   * 회원 가입
+   * MemberRequestDto : 가입 신청서
+   */
   @RequestMapping(value = "/api/member/signup", method = RequestMethod.POST)
   public ResponseDto<?> signup(@RequestBody @Valid MemberRequestDto requestDto) {
     return memberService.createMember(requestDto);
   }
 
+  /**
+   * 닉네임 중복 검사
+   * @param : 닉네임만
+   */
   @RequestMapping(value = "/api/member/signup/duplicate", method = RequestMethod.POST)
   public ResponseDto<?> checkDuplicate(@RequestBody @Valid DuplicateRequestDto requestDto) {
     return memberService.checkDuplicate(requestDto);
   }
 
-
-
+  /**
+   * 로그인
+   * @param : 닉네임 및 비밀번호
+   */
   @RequestMapping(value = "/api/member/login", method = RequestMethod.POST)
   public ResponseDto<?> login(@RequestBody @Valid LoginRequestDto requestDto,
       HttpServletResponse response
@@ -41,6 +51,9 @@ public class MemberController {
     return memberService.login(requestDto, response);
   }
 
+  /**
+   * 로그아웃
+   */
   @RequestMapping(value = "/api/member/logout", method = RequestMethod.POST)
   public ResponseDto<?> logout(HttpServletRequest request) {
     return memberService.logout(request);
